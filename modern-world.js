74e59b965079afb6db7ee639dd8daccd341b0e5e
Cameron Adamez <cameron@soycow.org>
2015-08-30 11:15:09 -0700

    Trying out enclosures

--- modern-world.js
+++ modern-world.js
@@ -1,60 +1,85 @@
-var ModernWorld = {};
-var Player = {};
-ModernWorld.canvas = document.getElementById('tutorial');
-ModernWorld.context = ModernWorld.canvas.getContext('2d');
-Player.img = new Image();
-Player.initial_pos = [400,350];
-Player.pos = [400,350];
+var ModernWorld = (function() {
+    var canvas = document.getElementById('tutorial');
+    var context = canvas.getContext('2d');
+    var fg_color = '#00FF00'; // foreground color
+    // coords is a list of tuples, eg [[0,0],[1,1]]
+    var draw = function (ctx, coords, start_point) {
+        if(start_point) {
+            coords = coords.map(function(coord){
+                return [coord[0] + start_point[0], coord[1] + start_point[1]]
+            });
+        }
+        ctx.beginPath();
+        ctx.moveTo(coords[0][0], coords[0][1]);
+        for (var h=1;h<coords.length;h++){
+            ctx.lineTo(coords[h][0], coords[h][1]);
+        }
+        ctx.closePath();
+        ctx.fillStyle = fg_color;
+        ctx.fill();
+    };
+     return {
+        init: function() {
+            Player.bounds = canvas.width;
+            Player.img.onload = function() {
+                //ctx.moveTo(0,0);
+                context.beginPath();
+                context.drawImage(Player.img,
+                                              Player.pos[0],
+                                              Player.pos[1]);
+            }
+            Player.img.src = 'player.png';
 
-ModernWorld.render = function() {
-    fg_color = '#00FF00'; // foreground color
-
-    Player.img.onload = function() {
-        //ctx.moveTo(0,0);
-        ModernWorld.context.beginPath();
-        ModernWorld.context.drawImage(Player.img,
-                                      Player.pos[0],
-                                      Player.pos[1]);
-    }
-    Player.img.src = 'player.png';
-
-    for(var i=1;i<=4;i++){
-        ModernWorld.draw_house(ModernWorld.context, [(100*i)+(50*i),200]);
-    }
-}
-
-ModernWorld.redraw_player = function (increment) {
-    ModernWorld.context.clearRect(0, Player.initial_pos[1], ModernWorld.canvas.width, ModernWorld.canvas.height);
-    Player.pos[0] = Player.pos[0] + increment;
-    ModernWorld.context.drawImage(Player.img,
+            for(var i=1;i<=4;i++){
+                ModernWorld.draw_house(context, [(100*i)+(50*i),200]);
+            }
+            //this.player_protest();
+        },
+        redraw_player: function (increment) {
+            // REFACTOR
+            Player.change_position(increment, canvas.width);
+            if (Player.has_changed_position){
+                context.clearRect(0, Player.initial_pos[1], canvas.width, canvas.height);
+                context.drawImage(Player.img,
                                   Player.pos[0],
                                   Player.pos[1]);
-}
+            }
+        },
+       player_protest: function() {
+            var p_x = Player.pos[0];
+            var p_y = Player.pos[1];
+            var coords = [ [p_x, p_y], [p_x+40, p_y], [p_x+40, p_y+30]
+            ];
+            draw(context, coords);
+        },
+        // Takes a 2d context, [x,y] array of coordinates, and a CSS color.
+        draw_house: function (ctx, start_point) {
+            var house_coords = [
+                [0, 20], [20, 0], [80,0], [100,20],  [100,100],
+                [60,100], [60,75], [40,75], [40,100], [0,100]
+            ];
+            draw(ctx, house_coords, start_point);
+        }
+     }
+})();
 
-
-// Takes a 2d context, [x,y] array of coordinates, and a CSS color.
-ModernWorld.draw_house = function (ctx, start_point) {
-    var house_coords = [
-        [0, 20], [20, 0], [80,0], [100,20],  [100,100],
-        [60,100], [60,75], [40,75], [40,100], [0,100]
-    ];
-    if(start_point) {
-        house_coords = house_coords.map(function(coord){
-            return [coord[0] + start_point[0], coord[1] + start_point[1]]
-        });
+var Player = {
+    img: new Image(),
+    initial_pos: [400,350],
+    pos: [400,350],
+    has_changed_position: false,
+    change_position: function(increment, bounds){
+        var newpos = Player.pos[0] + increment;
+        if (newpos < 10 ) { newpos = 0; this.has_changed_position = false;}
+        else if (newpos > bounds - 60)
+            { newpos = bounds - 60; this.has_changed_position = false;} //replace with width of image
+        else { this.has_changed_position = true; }
+        Player.pos[0] = newpos;
     }
-    ctx.beginPath();
-    ctx.moveTo(house_coords[0][0], house_coords[0][1]);
-    for (var h=1;h<house_coords.length;h++){
-        ctx.lineTo(house_coords[h][0], house_coords[h][1]);
-    }
-    ctx.closePath();
-    ctx.fillStyle = fg_color;
-    ctx.fill();
-}
+};
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    ModernWorld.render();
+    ModernWorld.init();
 });
 
 document.addEventListener("keydown", function (event) {
@@ -67,15 +92,12 @@ document.addEventListener("keydown", function (event) {
       // Do something for "down arrow" key press.
       break;
     case "ArrowUp":
-      // Do something for "up arrow" key press.
       break;
     case "ArrowLeft":
-        ModernWorld.redraw_player(-30);
-      // Do something for "left arrow" key press.
+      ModernWorld.redraw_player(-15);
       break;
     case "ArrowRight":
-        ModernWorld.redraw_player(30);
-      // Do something for "right arrow" key press.
+        ModernWorld.redraw_player(15);
       break;
     case "Enter":
       // Do something for "enter" or "return" key press.
