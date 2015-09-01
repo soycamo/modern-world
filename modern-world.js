e43d795762a21d4e8dd6ef53afb9b16f9d7832d5
Cameron Adamez <cameron@soycow.org>
2015-09-01 16:25:11 -0700

    Changed the 'draw' command, moved the Player into the closure, gave up on drawing the sign manually and added a PNG, and using requestAnimationFrame...

--- modern-world.js
+++ modern-world.js
@@ -3,7 +3,8 @@ var ModernWorld = (function() {
     var context = canvas.getContext('2d');
     var fg_color = '#00FF00'; // foreground color
     // coords is a list of tuples, eg [[0,0],[1,1]]
-    var draw = function (ctx, coords, start_point) {
+    // fill is a boolean. if false, it traces instead.
+    var draw = function (ctx, coords, fill, start_point) {
         if(start_point) {
             coords = coords.map(function(coord){
                 return [coord[0] + start_point[0], coord[1] + start_point[1]]
@@ -15,12 +16,33 @@ var ModernWorld = (function() {
             ctx.lineTo(coords[h][0], coords[h][1]);
         }
         ctx.closePath();
+        if (fill){
         ctx.fillStyle = fg_color;
         ctx.fill();
+        } else {
+        ctx.strokeStyle = fg_color;
+        ctx.stroke();
+        }
     };
-     return {
+    var Player = {
+        img: new Image(),
+        initial_pos: [400,350],
+        pos: [400,350],
+        has_changed_position: false,
+        change_position: function(increment, bounds){
+            var newpos = Player.pos[0] + increment;
+            if (newpos < 10 ) { newpos = 0; this.has_changed_position = false;}
+            else if (newpos > bounds - 60)
+                { newpos = bounds - 60; this.has_changed_position = false;} //replace with width of image
+            else { this.has_changed_position = true; }
+            Player.pos[0] = newpos;
+        }
+    };
+    Player.img.src = 'player.png';
+
+    var Bombs = [100,0];
+    return {
         init: function() {
-            Player.bounds = canvas.width;
             Player.img.onload = function() {
                 //ctx.moveTo(0,0);
                 context.beginPath();
@@ -28,12 +50,18 @@ var ModernWorld = (function() {
                                               Player.pos[0],
                                               Player.pos[1]);
             }
-            Player.img.src = 'player.png';
 
             for(var i=1;i<=4;i++){
                 ModernWorld.draw_house(context, [(100*i)+(50*i),200]);
             }
-            //this.player_protest();
+        },
+        play: function() {
+            //context.save();
+            Bombs[1] += 10;
+            context.fillRect(Bombs[0],Bombs[1],3,10);
+            ModernWorld.init();
+            //ModernWorld.redraw_player();
+            window.requestAnimationFrame(ModernWorld.play);
         },
         redraw_player: function (increment) {
             // REFACTOR
@@ -46,11 +74,15 @@ var ModernWorld = (function() {
             }
         },
        player_protest: function() {
-            var p_x = Player.pos[0];
-            var p_y = Player.pos[1];
-            var coords = [ [p_x, p_y], [p_x+40, p_y], [p_x+40, p_y+30]
-            ];
-            draw(context, coords);
+           // TODO make redraw and clear rect where sign used to be. similar to the way the player is handled
+           var img = new Image();
+           img.onload = function() {
+               context.beginPath();
+               context.drawImage(img,
+                                 Player.pos[0] + 25,
+                                 Player.pos[1] - 25);
+           }
+           img.src = 'sign.png';
         },
         // Takes a 2d context, [x,y] array of coordinates, and a CSS color.
         draw_house: function (ctx, start_point) {
@@ -58,52 +90,29 @@ var ModernWorld = (function() {
                 [0, 20], [20, 0], [80,0], [100,20],  [100,100],
                 [60,100], [60,75], [40,75], [40,100], [0,100]
             ];
-            draw(ctx, house_coords, start_point);
+            draw(ctx, house_coords, true, start_point);
         }
      }
 })();
 
-var Player = {
-    img: new Image(),
-    initial_pos: [400,350],
-    pos: [400,350],
-    has_changed_position: false,
-    change_position: function(increment, bounds){
-        var newpos = Player.pos[0] + increment;
-        if (newpos < 10 ) { newpos = 0; this.has_changed_position = false;}
-        else if (newpos > bounds - 60)
-            { newpos = bounds - 60; this.has_changed_position = false;} //replace with width of image
-        else { this.has_changed_position = true; }
-        Player.pos[0] = newpos;
-    }
-};
-
 document.addEventListener("DOMContentLoaded", function(event) {
     ModernWorld.init();
+    window.requestAnimationFrame(ModernWorld.play);
 });
 
 document.addEventListener("keydown", function (event) {
   if (event.defaultPrevented) {
     return; // Should do nothing if the key event was already consumed.
   }
-
   switch (event.key) {
-    case "ArrowDown":
-      // Do something for "down arrow" key press.
-      break;
-    case "ArrowUp":
+      case "x":
+          ModernWorld.player_protest();
       break;
     case "ArrowLeft":
       ModernWorld.redraw_player(-15);
       break;
     case "ArrowRight":
         ModernWorld.redraw_player(15);
-      break;
-    case "Enter":
-      // Do something for "enter" or "return" key press.
-      break;
-    case "Escape":
-      // Do something for "esc" key press.
       break;
     default:
       return; // Quit when this doesn't handle the key event.
