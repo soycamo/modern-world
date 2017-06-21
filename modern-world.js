88c0fffb6258d5a64f796e988df1af56b9b933a6
Cameron Adamez <cameron@soycow.org>
2017-06-21 11:31:42 -0600

    Bombs now fall down, animation can be paused. Instructions were added at the top. TODO: halt, clear, and redraw bombs, clear sign area.

--- modern-world.js
+++ modern-world.js
@@ -1,5 +1,16 @@
+window.requestAnimationFrame = window.requestAnimationFrame
+                               || window.mozRequestAnimationFrame
+                               || window.webkitRequestAnimationFrame
+                               || window.msRequestAnimationFrame
+                               || function(f){return setTimeout(f, 1000/60)}
+
+window.cancelAnimationFrame = window.cancelAnimationFrame
+                              || window.mozCancelAnimationFrame
+                              || function(requestID){clearTimeout(requestID)}
+
 var ModernWorld = (function() {
-    var canvas = document.getElementById('tutorial');
+    var ani_frame_ref;
+    var canvas = document.getElementById('screen');
     var context = canvas.getContext('2d');
     var fg_color = '#00FF00'; // foreground color
     // coords is a list of tuples, eg [[0,0],[1,1]]
@@ -17,11 +28,11 @@ var ModernWorld = (function() {
         }
         ctx.closePath();
         if (fill){
-        ctx.fillStyle = fg_color;
-        ctx.fill();
+            ctx.fillStyle = fg_color;
+            ctx.fill();
         } else {
-        ctx.strokeStyle = fg_color;
-        ctx.stroke();
+            ctx.strokeStyle = fg_color;
+            ctx.stroke();
         }
     };
     var Player = {
@@ -40,7 +51,31 @@ var ModernWorld = (function() {
     };
     Player.img.src = 'player.png';
 
-    var Bombs = [100,0];
+    var Bombs = {
+        bounds: [200,600],
+        counter: [200,0],
+        bomb_width: 3,
+        bomb_height: 10,
+        have_exceeded_bounds: false,
+        random_int: function(max, min){
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        fall_down: function (){
+            // all bombs descend at a rate, default is 5.
+
+            if (Bombs.counter[1] < 200) {
+                Bombs.have_exceeded_bounds = false;
+                Bombs.counter[1] += 5;
+            }
+            else {
+                // Should issue some kind of "bomb halt, erase and redraw"
+                Bombs.have_exceeded_bounds = true;
+                Bombs.counter[0] = Bombs.random_int(Bombs.bounds[0], Bombs.bounds[1]);
+                Bombs.counter[1] = 0;
+            }
+            return Bombs.counter;
+        },
+    };
     return {
         init: function() {
             Player.img.onload = function() {
@@ -56,12 +91,14 @@ var ModernWorld = (function() {
             }
         },
         play: function() {
-            //context.save();
-            Bombs[1] += 10;
-            context.fillRect(Bombs[0],Bombs[1],3,10);
+            context.save();
+            var bomb_pos = Bombs.fall_down();
+            console.log(bomb_pos);
+            // fillRect( x, y, width, height)
+            context.fillRect(bomb_pos[0],bomb_pos[1],Bombs.bomb_width,Bombs.bomb_height);
             ModernWorld.init();
             //ModernWorld.redraw_player();
-            window.requestAnimationFrame(ModernWorld.play);
+            ModernWorld.ani_frame_ref = window.requestAnimationFrame(ModernWorld.play);
         },
         redraw_player: function (increment) {
             // REFACTOR
@@ -97,7 +134,7 @@ var ModernWorld = (function() {
 
 document.addEventListener("DOMContentLoaded", function(event) {
     ModernWorld.init();
-    window.requestAnimationFrame(ModernWorld.play);
+    requestAnimationFrame(ModernWorld.play);
 });
 
 document.addEventListener("keydown", function (event) {
@@ -114,6 +151,10 @@ document.addEventListener("keydown", function (event) {
     case "ArrowRight":
         ModernWorld.redraw_player(15);
       break;
+    case "p":
+        console.log(ModernWorld.ani_frame_ref);
+        cancelAnimationFrame(ModernWorld.ani_frame_ref);
+        break;
     default:
       return; // Quit when this doesn't handle the key event.
   }
